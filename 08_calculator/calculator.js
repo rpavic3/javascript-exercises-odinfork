53f5966a9420c1947576541170580c7465a8a6b2
Robert Pavic <rpavic3@gmail.com>
2024-11-14 18:57:10 +0100

    solve eigth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,34 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b
+};
+
+const subtract = function(a, b) {
+	return a - b
+};
+
+const sum = function(arr) {
+  return arr.reduce((accu, iter) => accu + iter, 0);
+};
+
+
+const multiply = function(arr) {
+  return arr.reduce((accu, iter) => accu*iter, 1)
+
+};
+
+const power = function(a, b) {
+  return a**b
 	
 };
 
-const subtract = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1
+  let result = 1
+  for (let i = 1; i <= n; i++) {
+    result = result * i
+  }
+	return result
 };
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
 // Do not edit below this line
 module.exports = {
   add,

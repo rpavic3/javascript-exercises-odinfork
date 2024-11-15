c9350c5e6476e0f6726314138e99d6b36a765508
Robert Pavic <rpavic3@gmail.com>
2024-11-15 20:23:42 +0100

    finished 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    let splitStr = str.toLowerCase().split("")
+    splitStr = splitStr.filter((character) => alphanumerical.includes(character));
+    splitStr = splitStr.join("")
+
+
+    revStr = splitStr.split("").reverse().join("")
+
+
+    return revStr === splitStr
+
 
 };
 

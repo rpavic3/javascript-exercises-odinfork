4e4c28fa5e265fbb882bca78935eef0e1d9816ed
Robert Pavic <rpavic3@gmail.com>
2024-11-16 16:20:16 +0100

    solve 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
+
 
 };
-
+console.log(fibonacci("sd23sdaaa24123"))
 // Do not edit below this line
 module.exports = fibonacci;
